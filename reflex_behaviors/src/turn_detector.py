d50190872bf9e021508c96daf25db2315e22792d
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2018-08-13 18:29:18 +1200

    Add message SetIdleMotion
    Add option for leaning forward in idle motion
    
    Bug fixed

--- reflex_behaviors/src/turn_detector.py
+++ reflex_behaviors/src/turn_detector.py
@@ -3,7 +3,7 @@
 
 import rospy
 from std_msgs.msg import Bool, String, Empty
-from mind_msgs.msg import RaisingEvents
+from mind_msgs.msg import RaisingEvents, SetIdleMotion
 
 class TurnDetectorNode:
     def __init__(self):
@@ -13,22 +13,38 @@ class TurnDetectorNode:
         self.pub_start_speech_recognition = rospy.Publisher('sp_speech_recognizer/start', Empty, queue_size=1)
         self.pub_stop_speech_recognition = rospy.Publisher('sp_speech_recognizer/stop', Empty, queue_size=1)
 
-        self.pub_set_idle_motion = rospy.Publisher('set_enable_idle_motion', Bool, queue_size=10)
-        self.pub_set_idle_motion.publish(True)
+        self.pub_set_idle_motion = rospy.Publisher('set_enable_idle_motion', SetIdleMotion, queue_size=10)
 
+        msg = SetIdleMotion()
+        msg.enabled = True
+        msg.with_leaning_forward = False
+        self.pub_set_idle_motion.publish(msg)
 
         rospy.loginfo('\033[92m[%s]\033[0m initialized...'%rospy.get_name())
-        pass
 
     def handle_raising_events(self, msg):
         pass
 
     def handle_robot_is_saying(self, msg):
-        if msg.data:    # Robot started saying
-            self.pub_set_idle_motion.publish(False)
+        if msg.data:    
+            # Robot started saying
+            rospy.loginfo('\033[92m[%s]\033[0m Robot\'s Turn...')
+            
+            msg = SetIdleMotion()
+            msg.enabled = False
+            msg.with_leaning_forward = False
+
+            self.pub_set_idle_motion.publish(msg)
             self.pub_stop_speech_recognition.publish()
-        else:           # Robot completed saying
-            self.pub_set_idle_motion.publish(True)
+        else:           
+            # Robot completed saying
+            rospy.loginfo('\033[92m[%s]\033[0m User\'s Turn...')
+
+            msg = SetIdleMotion()
+            msg.enabled = True
+            msg.with_leaning_forward = True
+
+            self.pub_set_idle_motion.publish(msg)
             self.pub_start_speech_recognition.publish()
 
 
